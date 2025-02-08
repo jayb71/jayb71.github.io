498d0198368ac1b4755baaab2a74668a4f77d1ad
jayb71 <jaybharambe42@gmail.com>
2025-02-08 18:13:35 +0530

    fix image path

--- src/components/HomeScreen/MyPhoto.tsx
+++ src/components/HomeScreen/MyPhoto.tsx
@@ -4,7 +4,7 @@ import './HomeScreen.css'
 const MyPhoto:React.FC =() => {
   return (
     <div className="flex justify-center items-center">
-      <img src="/public/jay.jpg" alt="Jay Bharambe" className="rounded-full" />
+      <img src="jay.jpg" alt="Jay Bharambe" className="rounded-full" />
       <div className="absolute inset-0 bg-gradient-to-t from-black via-black/40"></div>
     </div>
   )
